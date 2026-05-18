a7476ac77be48a08e6cc197daba08a1f6a6f1e17
kavya-19cpu <kavyajain01934@gmail.com>
2026-05-18 23:20:28 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const app = express();
 
 app.use(express.static("public"));
 
-app.get("/products", (req, res) => {
+// ✅ HOME ROUTE (FIX for Cannot GET /)
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
+// API route
+app.get("/products", (req, res) => {
   const data = fs.readFileSync(
     path.join(__dirname, "public", "products.json"),
     "utf-8"
@@ -15,6 +20,8 @@ app.get("/products", (req, res) => {
 
   res.json(JSON.parse(data));
 });
+
+// PORT for Render
 const PORT = process.env.PORT || 45000;
 
 app.listen(PORT, () => {
